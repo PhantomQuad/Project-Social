d74f625f417ade525ccaf0f8d57ca2ba5ce6f402
Nathan <PhantonQuad@users.noreply.github.com>
2022-02-02 17:37:11 +0000

    backup

--- src/App.js
+++ src/App.js
@@ -11,51 +11,65 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
 function App() {
+  const [profile, changeProfile] = useState([]);
 
-  const [todos, changeTodos] = useState([]);
-
-  const updateList = (id, age, location, hobby, aboutme, photo) => {
-    const listItem = {id, age, location, hobby, aboutme, photo };
-    changeTodos((state) => [...state, listItem], localStorage.setItem("list",
-    JSON.stringify([...todos, listItem])
-    ))
+  const updateList = (username, age, location, hobby, aboutme, photo) => {
+    const listItem = { username, age, location, hobby, aboutme, photo };
+    changeProfile(
+      (state) => [...state, listItem],
+      localStorage.setItem("list", JSON.stringify([...profile, listItem]))
+    );
   };
+
+  useEffect(() => {
+    const listContents = localStorage.getItem("list");
+    changeProfile(JSON.parse(listContents) || []);
+  }, []);
 
   return (
     <BrowserRouter>
-      <Container >
-          <Navbar bg="success" variant="dark" expand="lg">
-            <Container>
-              <Navbar.Brand href="#home">
-                <img
-                  alt=""
-                  src="https://learning.thedeveloperacademy.com/pluginfile.php/1/theme_moove/logo/1624462331/TheDevAcademy%20Logo%20NB.png"
-                  width="50"
-                  height="30"
-                  className="d-inline-block align-top"
-                />{" "}
-                The Dating Site
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link as={Link} to="/post">
-                    Post
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/view">
-                    View
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+      <Container>
+        <Navbar bg="success" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src="https://learning.thedeveloperacademy.com/pluginfile.php/1/theme_moove/logo/1624462331/TheDevAcademy%20Logo%20NB.png"
+                width="50"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              The Dating Site
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/post">
+                  Post
+                </Nav.Link>
+                <Nav.Link as={Link} to="/view">
+                  View
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </Container>
 
       <Container className="app">
         <Routes>
-          <Route path="/post" element={<Post />} />
-          <Route path="/view" element={<View />} />
-          <Route index element={<View />} />
+          <Route
+            path="/post"
+            element={
+              <Post
+              addProfile={(username, age, location, hobby, aboutme, photo) =>
+                  updateList(username, age, location, hobby, aboutme, photo)
+                }
+              />
+            }
+          />
+          <Route path="/view" element={<View profile={profile} />} />
+          <Route index element={<View profile={profile} />} />
         </Routes>
       </Container>
     </BrowserRouter>
--- src/Post.js
+++ src/Post.js
@@ -4,41 +4,141 @@ import Button from "react-bootstrap/Button";
 import toastr from "toastr";
 import "toastr/build/toastr.min.css";
 
+toastr.options = {
+  closeButton: true,
+  debug: false,
+  extendedTimeOut: "3000",
+  hideDuration: "1000",
+  hideEasing: "linear",
+  hideMethod: "fadeOut",
+  newestOnTop: false,
+  onclick: null,
+  positionClass: "toast-top-full-width",
+  preventDuplicates: true,
+  progressBar: true,
+  showDuration: "3000",
+  showEasing: "swing",
+  showMethod: "fadeIn",
+  timeOut: "5000",
+};
+
 function Post(props) {
   const [state, changeState] = useState({
-    id: 1,
-    age: 16,
+    username: "",
+    age: "",
     location: "",
     hobby: "",
     aboutme: "",
     photo: "",
   });
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.addProfile(
+      state.username,
+      state.age,
+      state.location,
+      state.hobby,
+      state.aboutme,
+      state.photo
+    );
+    toastr.success("Your profile added successfully");
+    changeState({
+      username: "",
+      age: "",
+      location: "",
+      hobby: "",
+      aboutme: "",
+      photo: "",
+    });
+  };
+
+  const handleChange = (event) => {
+    const newState = { ...state };
+    if (event.target.name === "completed") {
+      newState[event.target.name] = !state.completed;
+    } else {
+      newState[event.target.name] = event.target.value;
+    }
+    changeState(newState);
+  };
+
   return (
     <div>
-      <p>
-        ID
-        <input type="number" />
-      </p>
-      <p>
-        Location
-        <input type="text" />
-      </p>
-      <p>
-        DOB <input type="date" />
-      </p>
-      <p>
-        Hobby
-        <textarea type="text" />
-      </p>
-      <p>
-        Aboutme
-        <textarea type="text" />
-      </p>
-      <p>
-        Photo
-        <input type="text" />
-      </p>
+      <Form onSubmit={(event) => submitHandler(event)}>
+        <Form.Group controlId="userName">
+          <Form.Label> Username</Form.Label>
+          <Form.Control
+            name="username"
+            type="text"
+            value={state.username}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="userAge">
+          <Form.Label> Age</Form.Label>
+          <Form.Control
+            name="age"
+            type="number"
+            value={state.age}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="userLocation">
+          <Form.Label> Location</Form.Label>
+          <Form.Control
+            name="location"
+            type="text"
+            value={state.location}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="userLocation">
+          <Form.Label> Date of Birth</Form.Label>
+          <Form.Control
+            name="dob"
+            type="date"
+            value={state.dob}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="userHobby">
+          <Form.Label> Hobbys</Form.Label>
+          <Form.Control
+            name="hobby"
+            type="text"
+            value={state.hobby}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="userAbout">
+          <Form.Label> About You</Form.Label>
+          <Form.Control
+            name="aboutme"
+            type="text"
+            value={state.aboutme}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="userPhoto">
+          <Form.Label> Photo</Form.Label>
+          <Form.Control
+            name="photo"
+            type="text"
+            value={state.photo}
+            onChange={(e) => handleChange(e)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
