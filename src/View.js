c0191ca73866b2cdf1c4c2b98d1985b0faa90233
Nathan <PhantonQuad@users.noreply.github.com>
2022-02-03 09:24:09 +0000

    backup

--- src/View.js
+++ src/View.js
@@ -2,13 +2,16 @@ import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image'
 
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function View(props){
     const buildCard = () => {
         return props.profile.map((current) => (
            <> 
-            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
             <Card.Body>
-            <Card.Img as={Image} src={current.photo} fluid={true} alt="Profile image" />
+            <Card.Img variant="top" as={Image} src={current.photo} fluid={true} alt="Profile image" />
               <Card.Title>{current.username}</Card.Title>
               <Card.Text>Age: {current.age}</Card.Text>
               <Card.Text>Location:{current.location}</Card.Text>
